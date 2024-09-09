28698785284a28e77e0ce4ccfd5e506bdb83ce26
yamadahideto <yahideto428@gmail.com>
2024-09-09 15:51:53 +0900

    GoogleAnalytics導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -47,7 +47,7 @@ class HomeController < ApplicationController
         correct_answer = "俺以外"
         session[:correct_count] += 1 if correct_answer == params[:user_choice]
       when 4
-        correct_answer = "俺"
+        correct_answer = "俺以外"
         session[:correct_count] += 1 if correct_answer == params[:user_choice] 
       when 5
         correct_answer = "ひさじゅやないかい！"
